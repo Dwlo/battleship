d066e16d70508013f8a92408f49ede3c35165152
Dw <lo.dawid@gmail.com>
2013-12-27 21:36:28 +0100

    improv: Tests for fire method.

--- test/battleship/test/core.clj
+++ test/battleship/test/core.clj
@@ -177,3 +177,43 @@
     (testing "5x5 matrice: Shoot enemy in cell: row=2 col=1"
       (let [actual (shoot-enemy 2 1 "plx" battlefield)]
         (is (= (:shot-by (get-cell 2 1 actual)) "plx" ))))))
+
+(deftest test-fire
+  (let [battlefield (atom [{:has-enemy? true  :shot-by "player1"}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by "player1"}
+                           {:has-enemy? true  :shot-by "player2"}
+                           {:has-enemy? true  :shot-by "player2"}
+                           {:has-enemy? true  :shot-by "player2"}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by :none}
+                           {:has-enemy? true  :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by "playerX"}
+                           {:has-enemy? true  :shot-by "playerX2"}
+                           {:has-enemy? true  :shot-by "playerX2"}
+                           {:has-enemy? true  :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? false :shot-by :none}
+                           {:has-enemy? true  :shot-by "player1"}
+                           {:has-enemy? true  :shot-by :none}])]
+    (testing "5x5 matrice: Fire on enemy located at row=0 col=0"
+      (let [actual (fire 0 0 "plx" battlefield)]
+        (is (= actual :failure))))
+
+    (testing "5x5 matrice: Fire on enemy located at row=0 col=1"
+      (let [actual (fire 0 1 "plx" battlefield)]
+        (is (= actual :failure))))
+
+    (testing "5x5 matrice: Fire on enemy located at row=0 col=2"
+      (let [actual (fire 0 2 "plx" battlefield)]
+        (is (= actual :success))))
+    ))
