bbea91ab3410cb51af3b988b98ae9a9e99b867f6
Dw <lo.dawid@gmail.com>
2013-12-28 03:56:40 +0100

    test: Test fix.

--- test/battleship/test/handler.clj
+++ test/battleship/test/handler.clj
@@ -7,7 +7,7 @@
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "<br/><h1>Battleship ... </h1> boom ! fire !"))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
