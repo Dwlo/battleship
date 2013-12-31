d2b0a0cb658ce8f79aac5bd4c0a55c0239c299fc
Dw <lo.dawid@gmail.com>
2013-12-31 20:40:52 +0100

    refacto: adding hiccup dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [ring/ring-json "0.2.0"]]
+                 [ring/ring-json "0.2.0"]
+                 [hiccup "1.0.4"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler battleship.handler/app}
   :profiles
