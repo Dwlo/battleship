d1fc0dd50c1ae1a3774f3c82edf9ad7eedb786d4
Dw <lo.dawid@gmail.com>
2016-01-29 23:20:56 +0100

    update of libraries

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject battleship "0.1.0-SNAPSHOT"
-  :description "Community game: Rest Battleship"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [ring/ring-json "0.2.0"]
-                 [hiccup "1.0.4"]
+  :description         "Community game: Rest Battleship"
+  :url                 "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure       "1.8.0"]
+                 [compojure                 "1.1.8"]
+                 [ring/ring-json            "0.3.1"]
+                 [hiccup                    "1.0.4"]
                  [javax.servlet/servlet-api "2.5"]]
-  :plugins [[lein-ring "0.8.8"]]
+  :plugins [[lein-ring "0.8.11"]]
   :ring {:handler battleship.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
