378f886ad27005577dfff9abec84869c187d7e18
Dw <lo.dawid@gmail.com>
2016-02-11 23:26:54 +0100

    cleanup

--- test/battleship/test/logic_test.clj
+++ test/battleship/test/logic_test.clj
@@ -1,8 +1,6 @@
 (ns battleship.test.logic-test
-  (:require [battleship
-             [battlefield :as core]
-             [logic :refer :all]]
-            [clojure.test :refer :all]))
+  (:require [battleship.logic :refer :all]
+            [clojure.test     :refer :all]))
 
 (deftest test-found-enemy?
   (let [battlefield [{:has-enemy? true :shot-by "playerX"} :b {:has-enemy? false :shot-by :none} :d :e :f :g :h :i :j :k {:has-enemy? true :shot-by :none} :m :n :o :p :q :r :s :t :u :v :w :x :y]]
@@ -55,97 +53,6 @@
     (testing "nxn matrice: Fire on enemy located at row=0 col=2"
       (let [actual (attack 0 2 "plx" battlefield)]
         (is (= actual :success))))))
-
-
-;; (deftest test-show-context
-;;   (testing "nxn matrices: 2 games context"
-;;     (let [actual (get-games-info (atom {"game-id1" (atom [{:has-enemy? true  :shot-by "player1"}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "player1"}
-;;                                                                {:has-enemy? true  :shot-by "player2"}
-;;                                                                {:has-enemy? true  :shot-by "player2"}
-;;                                                                {:has-enemy? true  :shot-by "player2"}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "playerX"}
-;;                                                                {:has-enemy? true  :shot-by "playerX2"}
-;;                                                                {:has-enemy? true  :shot-by "playerX2"}
-;;                                                                {:has-enemy? true  :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "player1"}])
-
-;;                                              "game-id2" (atom [{:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "pl3"}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "pl1"}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? true  :shot-by "pl1"}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}
-;;                                                                {:has-enemy? false :shot-by :none}])}))]
-;;       (is (= (count actual) 2))
-;;       (is (= (nth actual 0) {"game-id1" {"player1" 3 "player2" 3 "playerX" 1 "playerX2" 2 :status :running}}))
-;;       (is (= (nth actual 1) {"game-id2" {"pl3" 1 "pl1" 2 :status :over}})))))
-
-;; (deftest test-get-game-stats
-;;   (testing "nxn matrices: battlefield"
-;;     (let [actual (get-game-stats [{:has-enemy? true  :shot-by "player1"}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? true  :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? true  :shot-by "player1"}
-;;                                     {:has-enemy? true  :shot-by "player2"}
-;;                                     {:has-enemy? true  :shot-by "player2"}
-;;                                     {:has-enemy? true  :shot-by "player2"}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? true  :shot-by "playerX"}
-;;                                     {:has-enemy? true  :shot-by "playerX2"}
-;;                                     {:has-enemy? true  :shot-by "playerX2"}
-;;                                     {:has-enemy? true  :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? false :shot-by :none}
-;;                                     {:has-enemy? true  :shot-by "player1"}])]
-;;       (is (= actual {:score  {"player1"  3
-;;                               "player2"  3
-;;                               "playerX"  1
-;;                               "playerX2" 2}
-;;                      :status :running})))))
 
 (deftest test-is-game-over
   (testing "The game is not over."
